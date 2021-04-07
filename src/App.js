ed39ef39db58acd6516f1e07517ea0ad834a3b59
Swati S <swativs16@gmail.com>
2021-04-07 17:46:28 +0530

    blank page issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import HelloWorld from "./components/HelloWorld";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Hello World!!</h1>
-      </header>
+      <HelloWorld />
     </div>
   );
 }
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HelloWorld() {
+  return (
+    <div class="main_section">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default HelloWorld;
